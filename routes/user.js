28978abf77863be295eb3eb4985911e6e48344c4
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-04-26 16:57:15 +0530

    added routes for register and login

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
+// defining home page routes
 router.get("/", (req, res) => {
   res.render("home");
+});
+
+// defining routes for login and register page.
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
